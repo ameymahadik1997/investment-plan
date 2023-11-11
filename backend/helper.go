b42fd87d379e35cdfd131f1b85f0515387cc9974
ameymahadik1997 <ameymahadik1997@gmail.com>
2023-11-11 12:15:25 +0530

    Fixed negative case for the get single customer

--- backend/helper.go
+++ backend/helper.go
@@ -239,13 +239,23 @@ func getCustomerInformationById(context *gin.Context) {
 	db := dbConnect()
 
 	var users []investmentOutput
-	id := context.Param("id")
-	query := "SELECT id, year, month, salary_credited, saving, mutual_funds, reits, independent_share, recurring_deposit, gold, future_security, house_groceries, self_expense, unspent_money, unique_id FROM investment_output WHERE id = ?;"
-	rows, err := db.Query(query, id)
+	paramId := context.Param("id")
+	query := fmt.Sprintf("SELECT * FROM investment_output WHERE id = %s;", paramId)
+	var count int
+	err := db.QueryRow(query).Scan(&count)
+	errString := fmt.Sprintf("Error: %s", err)
+
+	if strings.Contains(errString, "no rows in result set") {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"Message": "Information Not Present"})
+		return
+	}
+	query = "SELECT id, year, month, salary_credited, saving, mutual_funds, reits, independent_share, recurring_deposit, gold, future_security, house_groceries, self_expense, unspent_money, unique_id FROM investment_output WHERE id = ?;"
+	rows, err := db.Query(query, paramId)
 	if err != nil {
 		context.IndentedJSON(http.StatusBadRequest, gin.H{"Message": "Information Not Present"})
 		return
 	}
+
 	defer rows.Close()
 
 	for rows.Next() {
